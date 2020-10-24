19e8a6ec3b05ef2f69e95cf95c7391483d6763ac
Cody Berlin <codyberlin@gmail.com>
2020-10-24 12:51:37 -0700

    updated config with proper metadata

--- gridsome.config.js
+++ gridsome.config.js
@@ -2,11 +2,11 @@
 // Learn more: https://gridsome.org/docs/config
 
 module.exports = {
-  siteName: "Portfolio",
-  siteUrl: `https://www.itsnwa.com`,
+  siteName: "Cody Berlin",
+  siteUrl: `https://codyberlin.com`,
   host: "0.0.0.0",
-  titleTemplate: "%s - NWA",
-  siteDescription: "Creative technologist",
+  titleTemplate: "%s - Me",
+  siteDescription: "Software Engineer",
   plugins: [
     {
       use: "@gridsome/source-filesystem",
